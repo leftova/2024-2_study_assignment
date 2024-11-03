8be2f3c930889e81167415c7e8fdb15ffcbc8aed
Leftova <cchoe0128@gmail.com>
2024-11-04 00:20:43 +0900

    Update 4_3_statistics_ans.cs

--- assignment4/4_3_statistics_ans.cs
+++ assignment4/4_3_statistics_ans.cs
@@ -20,7 +20,83 @@ namespace statistics
 
             int stdCount = data.GetLength(0) - 1;
             // ---------- TODO ----------
+             double mathMax = 0, mathMin = 0, scienceMax = 0, scienceMin = 0, englishMax = 0, englishMin = 0;
+
+            double[] totalScores = new double[stdCount];
+            string[] names = new string[stdCount];
+            double mathScore = 0;
             
+            for (int i = 1; i <= 5; i++)
+            {
+                names[i - 1] = data[i, 1];
+                mathScore += double.Parse(data[i,2]);
+                if (mathMax == 0 || mathMax < double.Parse(data[i,2]))
+                {
+                    mathMax = double.Parse(data[i,2]);
+                }
+                if (mathMin == 0 || mathMin > double.Parse(data[i,2]))
+                {
+                    mathMin = double.Parse(data[i,2]);
+                }
+                
+            }
+            mathScore /= 5;
+            Console.WriteLine("Math: " + mathScore);
+
+            double scienceScore = 0;
+            for (int i = 1; i <= 5; i++)
+            {
+                scienceScore += double.Parse(data[i,3]); 
+                if (scienceMax == 0 || scienceMax < double.Parse(data[i,3]))
+                {
+                    scienceMax = double.Parse(data[i,3]);
+                }
+                if (scienceMin == 0 || scienceMin > double.Parse(data[i,3]))
+                {
+                    scienceMin = double.Parse(data[i,3]);
+                }
+                
+            }
+            scienceScore /= 5;
+            Console.WriteLine("Math: " + scienceScore);
+
+            double englishScore = 0;
+            for (int i = 1; i <= 5; i++)
+            {
+                englishScore += double.Parse(data[i,4]); 
+                if (englishMax == 0 || englishMax < double.Parse(data[i,4]))
+                {
+                    englishMax = double.Parse(data[i,4]);
+                }
+                if (englishMin == 0 || englishMin > double.Parse(data[i,4]))
+                {
+                    englishMin = double.Parse(data[i,4]);
+                }
+                
+            }
+            englishScore /= 5;
+
+            
+
+            Console.WriteLine("Average scores: ");
+            Console.WriteLine("Math: " + englishScore);
+
+            // 돌아가면 된다는 마인드
+            Console.WriteLine("Max and min scores: ");
+            Console.WriteLine("Math: (" + mathMax + ", " + mathMin + ")");
+            Console.WriteLine("Science: (" + scienceMax + ", " + scienceMin + ")");
+            Console.WriteLine("English: (" + englishMax + ", " + englishMin + ")");
+            
+            var rankedStudents = totalScores
+                .Select((score, index) => new { Name = names[index], TotalScore = score })
+                .OrderByDescending(student => student.TotalScore)
+                .Select((student, rank) => new { student.Name, student.TotalScore, Rank = rank + 1 });
+
+            Console.WriteLine("\nStudents rank by total scores:");
+            foreach (var student in rankedStudents)
+            {
+                Console.WriteLine($"{student.Name}: {student.Rank}th");
+            }
             // --------------------
         }
     }
